2e147fd4b9e226fcb1a5a573357e66fad0056c6c
TimalkaK <timalkamk@gmail.com>
2024-04-10 20:19:46 +0100

    feat: Added tests for users controller

--- events-platform-api/src/controllers/users_controller.test.ts
+++ events-platform-api/src/controllers/users_controller.test.ts
@@ -37,85 +37,90 @@ describe("GET /users endpoint", () => {
     expect(res.statusCode).toEqual(200);
   });
 
-  test("books successfully returned as empty array when no data returned from the service", async () => {
+  test("users successfully returned as empty array when no data returned from the service", async () => {
     // Arrange
-    jest.spyOn(bookService, "getBooks").mockResolvedValue([]);
+    jest.spyOn(userService, "getUsers").mockResolvedValue([]);
     // Act
-    const res = await request(app).get("/api/v1/books");
+    const res = await request(app).get("/users");
 
     // Assert
     expect(res.body).toEqual([]);
     expect(res.body.length).toEqual(0);
   });
 
-  test("books successfully returned as array of books", async () => {
+  test("users successfully returned as array of users", async () => {
     // Arrange
 
     // NB the "as" to `Book[]` takes care of all the missing properties added by sequelize
     //    such as createdDate etc, that we don't care about for the purposes of this test
     jest
-      .spyOn(bookService, "getBooks")
-      .mockResolvedValue(dummyBookData as Book[]);
+      .spyOn(userService, "getUsers")
+      .mockResolvedValue(dummyUserData as User[]);
 
     // Act
-    const res = await request(app).get("/api/v1/books");
+    const res = await request(app).get("/users");
 
     // Assert
-    expect(res.body).toEqual(dummyBookData);
+    expect(res.body).toEqual(dummyUserData);
     expect(res.body.length).toEqual(2);
   });
 });
 
-describe("GET /api/v1/books/{bookId} endpoint", () => {
-  test("status code successfully 200 for a book that is found", async () => {
+describe("GET /users/{userId} endpoint", () => {
+  test("status code successfully 200 for a user that is found", async () => {
     // Arrange
-    const mockGetBook = jest
-      .spyOn(bookService, "getBook")
-      .mockResolvedValue(dummyBookData[1] as Book);
+    const mockGetUsers = jest
+      .spyOn(userService, "getUser")
+      .mockResolvedValue(dummyUserData[1] as User);
 
     // Act
-    const res = await request(app).get("/api/v1/books/2");
+    const res = await request(app).get("/users/2");
 
     // Assert
     expect(res.statusCode).toEqual(200);
   });
 
-  test("status code successfully 404 for a book that is not found", async () => {
+  test("status code successfully 404 for a user that is not found", async () => {
     // Arrange
 
     jest
-      .spyOn(bookService, "getBook")
+      .spyOn(userService, "getUser")
       // this is a weird looking type assertion!
       // it's necessary because TS knows we can't actually return unknown here
       // BUT we want to check that in the event a book is missing we return a 404
-      .mockResolvedValue(undefined as unknown as Book);
+      .mockResolvedValue(undefined as unknown as User);
     // Act
-    const res = await request(app).get("/api/v1/books/77");
+    const res = await request(app).get("/users/77");
 
     // Assert
     expect(res.statusCode).toEqual(404);
   });
 
-  test("controller successfully returns book object as JSON", async () => {
+  test("controller successfully returns user object as JSON", async () => {
     // Arrange
     jest
-      .spyOn(bookService, "getBook")
-      .mockResolvedValue(dummyBookData[1] as Book);
+      .spyOn(userService, "getUser")
+      .mockResolvedValue(dummyUserData[1] as User);
 
     // Act
-    const res = await request(app).get("/api/v1/books/2");
+    const res = await request(app).get("/users/2");
 
     // Assert
-    expect(res.body).toEqual(dummyBookData[1]);
+    expect(res.body).toEqual(dummyUserData[1]);
   });
 });
 
-describe("POST /api/v1/books endpoint", () => {
-  test("status code successfully 201 for saving a valid book", async () => {
+describe("POST /users endpoint", () => {
+  test("status code successfully 201 for saving a valid user", async () => {
     // Act
-    const res = await request(app)
-      .post("/api/v1/books")
-      .send({ bookId: 3, title: "Fantastic Mr. Fox", author: "Roald Dahl" });
+    const res = await request(app).post("/users").send({
+      userId: 3,
+      firstName: "Elizabeth",
+      lastName: "Windemere",
+      email: "e.windemere@gmail.com",
+      password: "grape",
+      permissionLevel: "non-staff",
+    });
 
     // Assert
     expect(res.statusCode).toEqual(201);
@@ -123,14 +128,14 @@ describe("POST /api/v1/books endpoint", () => {
 
   test("status code 400 when saving ill formatted JSON", async () => {
     // Arrange - we can enforce throwing an exception by mocking the implementation
-    jest.spyOn(bookService, "saveBook").mockImplementation(() => {
-      throw new Error("Error saving book");
+    jest.spyOn(userService, "createUser").mockImplementation(() => {
+      throw new Error("Error creating user");
     });
 
     // Act
     const res = await request(app)
-      .post("/api/v1/books")
-      .send({ title: "Fantastic Mr. Fox", author: "Roald Dahl" }); // No bookId
+      .post("/users")
+      .send({ firstName: "Serena", lastName: "Williams" }); // No userid
 
     // Assert
     expect(res.statusCode).toEqual(400);
