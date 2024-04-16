efee1d01b7acf3d2ba0bcffce56b2c6ef66809a9
TimalkaK <timalkamk@gmail.com>
2024-04-16 22:04:30 +0100

    feat: Created Event card component

--- events-platform-frontend/src/App.tsx
+++ events-platform-frontend/src/App.tsx
@@ -1,130 +1,69 @@
-import './App.css';
-import Navbar from './components/navbar';
-import Header from './components/header';
-import Footer from './components/footer';
-import { useState } from 'react';
-import Cat from './data/cat';
-import CatCard from './components/cat_card';
-import { v4 as uuidv4 } from 'uuid';
+import "./App.css";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { useState } from "react";
+import Event from "./data/event";
+import EventCard from "./components/event_card";
+import { v4 as uuidv4 } from "uuid";
 
 function App(): JSX.Element {
+  // JavaScript/TypeScript code can be inserted here!
 
-	// JavaScript/TypeScript code can be inserted here!
-
-	const catData : Array<Cat> = [
-		{
-			name: 'Little Miss Purrfect',
-			species: 'Cat',
-			favFoods: ['wet food', 'dry food'],
-			birthYear: 2016,
-		},
-		{
-			name: 'Mr Gwumpy',
-			species: 'Cat',
-			favFoods: ['caviar'],
-			birthYear: 2016,
-		},
-		{
-			name: 'Dopey',
-			species: 'Cat',
-			favFoods: ['bugs'],
-			birthYear: 2018,
-		},
-		{
-			name: 'Jumpy',
-			species: 'Scaredy Cat',
-			favFoods: ['not cucumbers!'],
-			birthYear: 2008,
-		},
-		{
-			name: 'Meowsalot',
-			species: 'Cat',
-			favFoods: ['tuna', 'catnip', 'celery'],
-			birthYear: 2012,
-		},
-		{
-			name: 'Cindy Clawford',
-			species: 'Cat',
-			favFoods: ['mice'],
-			birthYear: 2012,
-		},
-		{
-			name: 'Katy Purry',
-			species: 'Cat',
-			favFoods: ['cigarettes', 'coffee'],
-			birthYear: 2015,
-		},
-		{
-			name: 'Dr. Von Belly-Rub',
-			species: 'Cat',
-			favFoods: ['salt'],
-			birthYear: 2020,
-		},
-		{
-			name: 'Blobby',
-			species: 'Bird?',
-			favFoods: ['your soul'],
-			birthYear: 2020,
-		},
-		{
-			name: 'El Taco',
-			species: 'Cat',
-			favFoods: ['tequila', 'bar snax'],
-			birthYear: 1995,
-		},
-		{
-			name: 'Nibbles',
-			species: 'Hungry Cat',
-			favFoods: ['blankets', 'feet', 'tinsel'],
-			birthYear: 2003,
-		},
-		{
-			name: 'BoopySnoot',
-			species: 'Tiny Cat',
-			favFoods: ['milk'],
-			birthYear: 2021,
-		},
+  const eventData: Array<Event> = [
     {
-      name: "Captain Catface",
-      species: "Sea Cattain",
-      favFoods: ["fish, rum"],
-      birthYear: 2016,
-    }
-];
-catData.forEach(cat => cat.id = uuidv4());
-console.log(catData); // each cat should now have a unique ID
+      name: "Women's Circle",
+      description:
+        "An evening of Insights, Rituals, Sharing and Crystal Bowl Sound Healing.",
+      date: "25th April 2024",
+      time: "5-7pm",
+      price: 0,
+    },
+    {
+      name: "Intention Setting with Crystals",
+      description:
+        "An ideal workshop for those who want to learn how to work with crystal magic",
+      date: "4th May 2024",
+      time: "10-11am",
+      price: 0,
+    },
+    {
+      name: "Sound Scapes",
+      description:
+        "An immersive sonic journey guided by the high vibrational frequencies of the gong, alchemic crystal bowls, shamanic drums, chimes, bells, and other ancient therapeutic instruments.",
+      date: "10th May 2024",
+      time: "7-8pm",
+      price: 0,
+    },
+  ];
+  eventData.forEach((event) => (event.id = uuidv4()));
+  console.log(eventData); // each cat should now have a unique ID
 
+  const [events, setEvents] = useState<Array<Event>>(eventData);
 
-	
-	const [ cats, setCats ] = useState<Array<Cat>>(catData);
-
-	const catCount = cats.length;
-
-	console.log("Our pretties 😻: ", catCount);
-
-	return (
-		<>
-			<Navbar />
-			<Header catCount={catCount}></Header>
-
-			<main>
-				<div className='cards__wrapper'>
-					{cats.map((cat, index) => (
-						<CatCard
-						key={cat.id}
-						name={cat.name}
-						species={cat.species}
-						favFoods={cat.favFoods}
-						birthYear={cat.birthYear}
-						catIndex={index}
-						/>
-						))};
-				</div>
-			</main>
-
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      {/*<Header catCount={catCount}></Header>*/}
+      <main>
+        <div className="cards__wrapper">
+          {events.map((event, index) => (
+            <EventCard
+              key={event.id}
+              name={event.name}
+              description={event.description}
+              date={event.date}
+              time={event.time}
+              price={event.price}
+              eventIndex={index}
+            />
+          ))}
+          ;
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ events-platform-frontend/src/data/event.ts
@@ -0,0 +1,8 @@
+export default interface Event {
+  id?: string;
+  name: string;
+  description: string;
+  date: string;
+  time: string;
+  price: number;
+}
